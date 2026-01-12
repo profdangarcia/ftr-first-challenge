a73708975ec944ebd218a9359548df6454415431
Dan Garcia <daniel.of@gmail.com>
2026-01-12 15:15:34 -0300

    web: big bang

---
+++ web/src/app.tsx
@@ -0,0 +1,7 @@
+export function App() {
+  return (
+    <main className="h-dvh flex flex-col items-center justify-center p-10">
+      <h1 className="text-4xl font-bold">Brev.ly</h1>
+    </main>
+  );
+}
